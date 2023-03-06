b3443f2c0b1668a392af72a1a617af86dcb1a311
carlos vilela <cvilela@uoc.edu>
2023-03-06 16:51:41 -0500

    my fisrts commit

---
+++ src/main/java/pruebas/Ejemplos.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package pruebas;
+
+
+import java.util.*;
+
+
+/**
+ * @author User
+ *
+ */
+public class Ejemplos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message = "Hello world!!rrr gt";
+		String newMessage = message.substring(6, 12)
+			     + message.substring(16,19);
+		
+		System.out.println(newMessage );
+	}	
+
+}
