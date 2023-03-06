1861755848f6ca95a57f050705cf51a7c77dfe7b
carlosVilelaG <97269311+carlosVilelaG@users.noreply.github.com>
2023-03-06 18:51:56 -0500

    Se agrega ajuste pruebas

--- src/main/java/pruebas/Ejemplos.java
+++ src/main/java/pruebas/Ejemplos.java
@@ -18,7 +18,7 @@ public class Ejemplos {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String message = "Hello world!!rrr gt";
+		String message = "Hello world!!rrr gtt";
 		String newMessage = message.substring(6, 12)
 			     + message.substring(16,19);
 		
